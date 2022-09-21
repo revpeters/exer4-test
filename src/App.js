a05520e192e7669befce438bba77894e0d2b0b78
revpeters <jacob.peters@aol.com>
2022-09-21 11:50:05 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I have built a build and deployment pipeline with github actions.
         </p>
         <a
           className="App-link"
